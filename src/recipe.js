aaf2354f07f02b15972250b44764795e635fa59b
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2022-04-21 02:54:10 +0900

    ⚡️ レシピ機能を追加 propsでファイルを分割しました。

---
+++ src/recipe.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Recipe(props) {
+    return (
+        <div>
+            <h1>
+                {props.title}
+            </h1>
+            <ol>
+                {props.ingredients.map(ingredients => (
+                    <li>
+                        {ingredients.text}
+                    </li>
+                ))}
+            </ol>
+            <p>{props.calories} cal</p>
+            <img src={props.image} alt="" />
+        </div>
+    )
+}
+
+export default Recipe;
